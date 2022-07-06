e1802214b266f74fd7171473086a7b87dc16f2e7
zülfü balkan <zulfubalkan@hotmail.com>
2022-07-06 07:43:54 +0300

    Last changes before UI

--- src/studentDataBaseApp/StudentDatabaseApp.java
+++ src/studentDataBaseApp/StudentDatabaseApp.java
@@ -1,16 +1,28 @@
 package studentDataBaseApp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		
-		Student newStudent = new Student();
-		newStudent.enrollCourse();
-		newStudent.payTuition();
-		System.out.println(newStudent.toString());
 		//Ask How many student will added to course
+		System.out.print("Enter the number of new students : ");
+		Scanner scan = new Scanner(System.in);
+		int numsofStudent = scan.nextInt();
+		Student[] students = new Student[numsofStudent];
 		
 		//Create N number of student
+		for(int n=0;n<numsofStudent;n++) {
+			students[n] = new Student();
+			students[n].enrollCourse();
+			students[n].payTuition();
+			System.out.println(students[n].toString());
+		}
+		//Total Info of Students
+		for(int i =0;i<numsofStudent;i++) {
+			System.out.println(students[i].toString());
+		}
 
 	}
 
