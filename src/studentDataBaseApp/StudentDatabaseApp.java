bcaca9b3ea39cf61f590b51f5f5fa91309811581
zülfü balkan <zulfubalkan@hotmail.com>
2022-07-06 10:18:28 +0300

    Revert "UI implementation for input"
    This reverts commit c7682a2b78010e1c74916c7c66f5e79a56eb2420.

--- src/studentDataBaseApp/StudentDatabaseApp.java
+++ src/studentDataBaseApp/StudentDatabaseApp.java
@@ -1,15 +1,28 @@
 package studentDataBaseApp;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Scanner;
 
+public class StudentDatabaseApp {
 
-public class StudentDatabaseApp{
-
-	public static void main (String[] args) {
-		GUI.GUI();
+	public static void main(String[] args) {
+		
+		//Ask How many student will added to course
+		System.out.print("Enter the number of new students : ");
+		Scanner scan = new Scanner(System.in);
+		int numsofStudent = scan.nextInt();
+		Student[] students = new Student[numsofStudent];
+		
+		//Create N number of student
+		for(int n=0;n<numsofStudent;n++) {
+			students[n] = new Student();
+			students[n].enrollCourse();
+			students[n].payTuition();
+			System.out.println(students[n].toString());
+		}
+		//Total Info of Students
+		for(int i =0;i<numsofStudent;i++) {
+			System.out.println(students[i].toString());
+		}
 
 	}
 
